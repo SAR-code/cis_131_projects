6cf88a7e0203a39f0f2ad853ad30483dbbab3d36
SAR-code <dylanmccallum28@gmail.com>
2024-09-01 23:59:03 -0700

    mpg file init

---
+++ class_projects/cis_131_mpg.py
@@ -0,0 +1,10 @@
+#cis_131_mpg.py
+'''
+script: cis_131_mpg.py
+action: A. Receives user input on miles driven and gallons used per fill 
+        B. Calculates and displays the mpg obtained after each tankful
+        C. Calculate and display combined mpg for all tankfuls
+author: Dylan McCallum
+date: 02SEP24
+
+'''
