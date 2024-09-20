f5c01a11e01cc3a684c7fc778a21548422be89b0
SAR-code <dylanmccallum28@gmail.com>
2024-09-19 18:32:26 -0700

    Class notes, may access on mac

---
+++ class_projects/cis_131_craps_game.py
@@ -0,0 +1,45 @@
+'''
+script: cis_131_craps_game.py
+action: The script simulates the dice game craps. The script keeps track
+        of the win/lose percentages and calculates the updated percentage
+        per roll.
+author: Dylan McCallum
+date: 19SEP24
+'''
+
+# imports the required modules
+import random
+# declare main function to house the other functions
+def main():
+    print("Hello from main")
+# declare function to roll the dice
+
+# declare function to display the dice roll
+
+# declare function to calculate sum of dice
+
+# determine sum based on first roll
+
+# continue rolling until player wins or loses
+
+# display win or lose message
+
+# invokes main function to run the application
+main()
+
+'''
+Notes to consider, erase when complete
+
+dictionaries for my_wins and my_losses {1:0, 2:0, 25:0}
+dictionaries for my_results 
+GAMES_PLAYED = 10000
+game_roll_count
+
+GOST
+
+Goals -> Qualitative
+Objectives -> Quantitative
+Strategies -> Qualitative
+Tactics -> Quantitative
+
+'''
---
+++ class_projects/class_notes_17.py
@@ -0,0 +1,69 @@
+'''
+script: class_notes_17sep.py
+action: runs my class notes for the day
+author: Dylan McCallum  
+date: 05SEP24
+
+'''
+
+# notes
+
+# Dictionaries
+
+'''
+Dictionaries have key-value pairs. Consists of a list of key-value pairs
+
+'''
+
+my_book = { "Chapter 1": "The Beginning",
+            "Chapter 2": "The Start",
+            "Chapter 3": ["Two Chapters Here", "Another Chapter"],
+            4.56: 'Float'
+            }
+
+#print(my_book)
+
+
+print(my_book)
+
+my_book["Chapter 3"] = my_book[1] = 'Changed Chapter'
+
+print(my_book)
+
+for item in my_book.keys():
+    print(type(my_book[item]))
+    
+for value in my_book.values():
+    print(value)
+    
+'''
+Finite state machines
+
+    fs1 -------------> fs2
+    
+'''
+
+# Lisy Comprehensions
+list1 = []
+for item in range(1,6):
+    list1.append(item)
+    
+list2 = list(range(1,6))
+
+list3 = [item**3 for item in range(1,6)]
+
+list4 = [item for item in range(1,11) if item%2 == 0]
+
+print(list2, list3, list4)
+
+colours = ['red','orange','yellow','green','blue']
+
+colors = [item.upper() for item in colours]
+
+print(colours, colors)
+
+grades = {'Sue': [98, 87, 94], 'Bob': [84, 95, 91]}
+
+grades2 = {k: sum(v) / len(v) for k, v in grades.items()}
+
+print(grades2)
